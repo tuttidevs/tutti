4a714b9272070859de586f73d69fcdf3cfec8524
Colin Heffernan <92215668+colin-heffernan@users.noreply.github.com>
2026-03-31 19:53:06 -0400

    Implement auth and sessions (#5)
    * Add .node-version
    * Update auth setup
    * Move .node-version to correct dir
    * Migrate to native Django auth
    * Implement sessions
    * Redirect user with existing session to home

--- tutti/api/urls.py
+++ tutti/api/urls.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import UserView, CreateUserView
+from .views import TuttiUserView, CreateTuttiUserView, LoginTuttiUserView, LogoutTuttiUserView, CsrfView, TuttiUserSessionView
 
 urlpatterns = [
-    path('users', UserView.as_view()),
-    path('auth/register', CreateUserView.as_view()),
+    path('users/', TuttiUserView.as_view()),
+    path('csrf/', CsrfView.as_view()),
+    path('auth/register/', CreateTuttiUserView.as_view()),
+    path('auth/login/', LoginTuttiUserView.as_view()),
+    path('auth/logout/', LogoutTuttiUserView.as_view()),
+    path('auth/session/', TuttiUserSessionView.as_view())
 ]
